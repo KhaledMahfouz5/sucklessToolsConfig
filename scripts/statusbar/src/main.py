7d76d33982f6353d3d7c8ef36547bfcdb53bdc64
KhaledMahfouz5 <khaledmhfz2004@gmail.com>
2024-08-31 10:34:18 +0300

    new file:   getxkblayout/getxkblayout 	new file:   getxkblayout/getxkblayout.c 	modified:   scripts/statusbar/src/main.py 	Add KeyBoard Layout indicator ;)

--- scripts/statusbar/src/main.py
+++ scripts/statusbar/src/main.py
@@ -3,6 +3,9 @@ from hijridate import Hijri, Gregorian
 import time
 
 while True:
+    # Get Current Keyboard Layout
+    lang = subprocess.run(['bash', '-c', "$HOME/sucklessToolsConfig/getxkblayout/getxkblayout | awk 'NR==1 {print $3}'"],capture_output=True,text=True)
+    lang = lang.stdout.strip()
     # Get Formatted Date
     formatted_date = Hijri.today().isoformat()+" -> "+Gregorian.today().isoformat()
     # Get Current Time
@@ -12,6 +15,6 @@ while True:
     volume = subprocess.run(['bash', '-c', 'wpctl get-volume @DEFAULT_AUDIO_SINK@ | awk \'{print int($2 * 100) "% " $3}\''],capture_output=True,text=True)
     volume = volume.stdout.strip()
     # Make Status Text
-    status = f" dwm-6.5 | Vol : {volume} | Date : {formatted_date} | Time : {current_time} "
+    status = f" lang : {lang} | Vol : {volume} | Date : {formatted_date} | Time : {current_time} "
     subprocess.run(["xprop", "-root", "-set", "WM_NAME", status])
     time.sleep(1)
