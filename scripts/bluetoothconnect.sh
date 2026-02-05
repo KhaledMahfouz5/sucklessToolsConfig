6b5c710e636652f2973c96f6ecb4b9239dbbb4da
Khaled Mahfouz <khaledmhfz2004@gmail.com>
2026-02-06 00:04:54 +0300

    update bluetooth scripts
    turning bluetooth off once before connecting
    and another after disconnecting ..
    it's seems funnty .. but it works !!
    
    >> FileTree Changes :
            modified:   scripts/bluetoothconnect.sh
            modified:   scripts/bluetoothdisconnect.sh
    
    Signed Off By : Khaled Mahfouz

--- scripts/bluetoothconnect.sh
+++ scripts/bluetoothconnect.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 doas /usr/sbin/rfkill unblock all
+bluetoothctl power off
 bluetoothctl power on
 bluetoothctl connect $(cat ~/.bluetooth) # add your bluetooth MAC in '~/.bluetooth' file
--- scripts/bluetoothdisconnect.sh
+++ scripts/bluetoothdisconnect.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
 bluetoothctl disconnect
+bluetoothctl power off
