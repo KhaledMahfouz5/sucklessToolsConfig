12f2d9a62432105d0f293d9aab95b79407dd0098
Khaled Mahfouz <khaledmhfz2004@gmail.com>
2026-03-12 23:50:36 +0300

    Refactor screen recording script to improve organization and flexibility

--- scripts/record-screen.sh
+++ scripts/record-screen.sh
@@ -1,2 +1,13 @@
 #!/bin/bash
-ffmpeg -f x11grab -video_size 1920x1080 -framerate 25 -i :0.0 -vcodec libx264 output.mp4
+
+# initialize a unified directory for recordings
+mkdir -p "$HOME/Screenrecordings"
+
+# Configuration
+TIMESTAMP=$(date +"%d%m%Y-%H%M")
+OUTPUT="$HOME/Screenrecordings/record-$TIMESTAMP.mp4"
+VIDEO_SIZE="1920x1080"
+FRAMERATE="25"
+DISPLAY=":0.0"
+
+ffmpeg -f x11grab -video_size $VIDEO_SIZE -framerate $FRAMERATE -i $DISPLAY -vcodec libx264 $OUTPUT
